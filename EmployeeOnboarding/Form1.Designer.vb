2f9f2a63ba53be04f90a47a594022aff9d04121e
AnaGutierrezfiu01 <aguti290@fiu.edu>
2026-01-14 17:13:58 -0500

    Initial WinForms onboarding portal draft
    Early MVP to align on structure and next steps.

---
+++ EmployeeOnboarding/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEmployeeOnboarding
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.radIntern = New System.Windows.Forms.RadioButton()
+        Me.radFullTime = New System.Windows.Forms.RadioButton()
+        Me.dtpStartDate = New System.Windows.Forms.DateTimePicker()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.cboDepartment = New System.Windows.Forms.ComboBox()
+        Me.lstEmployees = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Department:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Role:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Start Date:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(31, 198)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Employees:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(101, 45)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(163, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'radIntern
+        '
+        Me.radIntern.AutoSize = True
+        Me.radIntern.Location = New System.Drawing.Point(103, 118)
+        Me.radIntern.Name = "radIntern"
+        Me.radIntern.Size = New System.Drawing.Size(52, 17)
+        Me.radIntern.TabIndex = 2
+        Me.radIntern.TabStop = True
+        Me.radIntern.Text = "Intern"
+        Me.radIntern.UseVisualStyleBackColor = True
+        '
+        'radFullTime
+        '
+        Me.radFullTime.AutoSize = True
+        Me.radFullTime.Location = New System.Drawing.Point(163, 118)
+        Me.radFullTime.Name = "radFullTime"
+        Me.radFullTime.Size = New System.Drawing.Size(67, 17)
+        Me.radFullTime.TabIndex = 3
+        Me.radFullTime.TabStop = True
+        Me.radFullTime.Text = "Full-Time"
+        Me.radFullTime.UseVisualStyleBackColor = True
+        '
+        'dtpStartDate
+        '
+        Me.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpStartDate.Location = New System.Drawing.Point(101, 150)
+        Me.dtpStartDate.MaxDate = New Date(3100, 12, 31, 0, 0, 0, 0)
+        Me.dtpStartDate.MinDate = New Date(2026, 1, 1, 0, 0, 0, 0)
+        Me.dtpStartDate.Name = "dtpStartDate"
+        Me.dtpStartDate.Size = New System.Drawing.Size(163, 20)
+        Me.dtpStartDate.TabIndex = 4
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(149, 365)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(90, 23)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(53, 365)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(90, 23)
+        Me.btnAdd.TabIndex = 5
+        Me.btnAdd.Text = "Add Employee"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'cboDepartment
+        '
+        Me.cboDepartment.FormattingEnabled = True
+        Me.cboDepartment.Location = New System.Drawing.Point(101, 81)
+        Me.cboDepartment.Name = "cboDepartment"
+        Me.cboDepartment.Size = New System.Drawing.Size(163, 21)
+        Me.cboDepartment.TabIndex = 1
+        '
+        'lstEmployees
+        '
+        Me.lstEmployees.FormattingEnabled = True
+        Me.lstEmployees.Location = New System.Drawing.Point(34, 223)
+        Me.lstEmployees.Name = "lstEmployees"
+        Me.lstEmployees.Size = New System.Drawing.Size(230, 134)
+        Me.lstEmployees.TabIndex = 16
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.Label6.Location = New System.Drawing.Point(11, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(269, 27)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Employee Onboarding Portal"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmEmployeeOnboarding
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Azure
+        Me.ClientSize = New System.Drawing.Size(292, 400)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lstEmployees)
+        Me.Controls.Add(Me.cboDepartment)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.dtpStartDate)
+        Me.Controls.Add(Me.radFullTime)
+        Me.Controls.Add(Me.radIntern)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmEmployeeOnboarding"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Employee Onboarding"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents radIntern As RadioButton
+    Friend WithEvents radFullTime As RadioButton
+    Friend WithEvents dtpStartDate As DateTimePicker
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents cboDepartment As ComboBox
+    Friend WithEvents lstEmployees As ListBox
+    Friend WithEvents Label6 As Label
+End Class
